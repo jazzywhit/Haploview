0a29a1490dd519e3e8dc85b95f0b675526df88ee
jcbarret <jcbarret>
2003-02-11 18:06:46 +0000

    data type used to hold info about a 2-marker comparison

---
+++ edu/mit/wi/haploview/PairwiseLinkage.java
@@ -0,0 +1,44 @@
+package edu.mit.wi.haploview;
+
+class PairwiseLinkage{
+
+    private double dprime, lod, r2, ci_low, ci_high;
+    private double[] freqs;
+
+    PairwiseLinkage(double d, double l, double r, double lo, double hi, double[] f){
+	dprime = d;
+	lod = l;
+	r2 = r;
+	ci_low = lo;
+	ci_high = hi;
+	freqs = f;
+    }
+
+    public double getDPrime(){
+	return dprime;
+    }
+
+    public double getLOD(){
+	return lod;
+    }
+    
+    public double getRSquared(){
+	return r2;
+    }
+
+    public double getConfidenceLow(){
+	return ci_low;
+    }
+
+    public double getConfidenceHigh(){
+	return ci_high;
+    }
+
+    public double[] getFreqs(){
+	return freqs;
+    }
+
+    public String toString(){
+	return new String(dprime + "\t" + lod + "\t" + r2 + "\t" + ci_low + "\t" + ci_high);
+    }
+}
