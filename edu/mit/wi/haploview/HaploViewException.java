c6602a1fcb6c127b8cdd99ec6f22b691cb792ee1
jmaller <jmaller>
2003-07-30 21:32:25 +0000

    renamed inputconflictexception to haploviewexception

---
+++ edu/mit/wi/haploview/HaploViewException.java
@@ -0,0 +1,7 @@
+package edu.mit.wi.haploview;
+
+public class HaploViewException extends Exception {
+         HaploViewException(String s){
+             super(s);
+         }
+}
--- edu/mit/wi/haploview/InputConflictException.java
+++
@@ -1,7 +0,0 @@
-package edu.mit.wi.haploview;
-
-public class InputConflictException extends Exception {
-         InputConflictException(String s){
-             super(s);
-         }
-}
