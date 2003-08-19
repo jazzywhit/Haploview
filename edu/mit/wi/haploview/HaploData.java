8355dd9a849b7690ed54e335aed2cc8ab11a29c9
jcbarret <jcbarret>
2003-08-19 18:56:54 +0000

    bugfix for loading info when data already loaded

--- edu/mit/wi/haploview/HaploData.java
+++ edu/mit/wi/haploview/HaploData.java
@@ -167,6 +167,7 @@ public class HaploData{
                         }
                     }
                 }
+                filteredDPrimeTable = getFilteredTable();
             }
             infoKnown=true;
         }else{
