b36332dac2a39670cc49670403772e8f019c0518
jcbarret <jcbarret>
2004-01-09 18:49:52 +0000

    straightened out current scheme

--- edu/mit/wi/haploview/DPrimeDisplay.java
+++ edu/mit/wi/haploview/DPrimeDisplay.java
@@ -16,7 +16,6 @@ class DPrimeDisplay extends JComponent implements MouseListener, MouseMotionList
     private static final int STD_SCHEME = 1;
     private static final int SFS_SCHEME = 2;
     private static final int GAM_SCHEME = 3;
-    int currentScheme;
 
     private static final int BOX_SIZES[] = {50, 24, 12};
     private static final int BOX_RADII[] = {24, 11, 6};
@@ -167,7 +166,6 @@ class DPrimeDisplay extends JComponent implements MouseListener, MouseMotionList
                 }
             }
         }
-        currentScheme = scheme;
     }
 
     public Image export(Image i){
