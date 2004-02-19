a996899a1bb42a67f6f19a11312521967e95b8b2
jcbarret <jcbarret>
2004-02-19 21:02:29 +0000

    *** empty log message ***

---
+++ edu/mit/wi/haploview/Constants.java
@@ -0,0 +1,37 @@
+package edu.mit.wi.haploview;
+
+import java.util.Hashtable;
+
+
+public interface Constants {
+
+    //main jframe setup stuff & labels.
+    public static final String TITLE_STRING = "Haploview v2.04";
+    public static final String READ_GENOTYPES = "Open genotype data";
+    public static final String READ_MARKERS = "Load marker data";
+
+    public static final String EXPORT_TEXT = "Export current tab to text";
+    public static final String EXPORT_PNG = "Export current tab to PNG";
+    public static final String EXPORT_OPTIONS = "Export options";
+
+    public static final String CLEAR_BLOCKS = "Clear all blocks";
+    public static final String CUST_BLOCKS = "Customize Block Definitions";
+
+    public static final String VIEW_DPRIME = "D Prime Plot";
+    public static final String VIEW_HAPLOTYPES = "Haplotypes";
+    public static final String VIEW_LOD = "Genotyping completeness";
+    public static final String VIEW_CHECK_PANEL = "Check Markers";
+    public static final String VIEW_TDT = "Association";
+
+    //main frame tab numbers
+    public static final int VIEW_D_NUM = 0;
+    public static final int VIEW_HAP_NUM = 1;
+    public static final int VIEW_CHECK_NUM = 2;
+    public static final int VIEW_TDT_NUM = 3;
+
+    //export modes
+    public static final int PNG_MODE = 0;
+    public static final int TXT_MODE = 1;
+    public static final int TABLE_TYPE = 0;
+    public static final int LIVE_TYPE = 1;
+}
