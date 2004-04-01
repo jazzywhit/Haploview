8762e9196d34a88e006358cc6bf0a561db5f798d
jcbarret <jcbarret>
2004-04-01 22:20:20 +0000

    added minor and major allele fields

--- edu/mit/wi/haploview/SNP.java
+++ edu/mit/wi/haploview/SNP.java
@@ -5,11 +5,14 @@ class SNP{
     private String name;
     private long position;
     private double MAF;
+    private byte minor, major;
 
-    SNP(String n, long p, double m){
+    SNP(String n, long p, double m, byte a1, byte a2){
         name = n;
         position = p;
         MAF = m;
+        major = a1;
+        minor = a2;
     }
 
     public String getName(){
@@ -22,5 +25,13 @@ class SNP{
 
     public double getMAF(){
         return MAF;
+    }
+
+    public byte getMinor(){
+        return minor;
+    }
+
+    public byte getMajor(){
+        return major;
     }
 }
