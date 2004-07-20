84877eec07a9cb2d0dbd2ffef25b8a54ae5c9375
jcbarret <jcbarret>
2004-07-20 18:33:42 +0000

    merged fitzhuwm_mods_070904

--- edu/mit/wi/haploview/SNP.java
+++ edu/mit/wi/haploview/SNP.java
@@ -5,6 +5,9 @@ class SNP{
     private String name;
     private long position;
     private double MAF;
+    private String extra;
+    private String gene_accession;
+    private String gene_symbol;
     private byte minor, major;
 
     SNP(String n, long p, double m, byte a1, byte a2){
@@ -13,6 +16,17 @@ class SNP{
         MAF = m;
         major = a1;
         minor = a2;
+    }
+
+    SNP(String n, long p, double m, byte a1, byte a2, String a, String s, String e){
+        name = n;
+        position = p;
+        MAF = m;
+        major = a1;
+        minor = a2;
+        gene_accession = a;
+        gene_symbol = s;
+        extra = e;
     }
 
     public String getName(){
@@ -25,6 +39,18 @@ class SNP{
 
     public double getMAF(){
         return MAF;
+    }
+
+    public String getExtra(){
+	return extra;
+    }
+
+    public String getGeneAccession(){
+	return gene_accession;
+    }
+
+    public String getGeneSymbol(){
+	return gene_symbol;
     }
 
     public byte getMinor(){
