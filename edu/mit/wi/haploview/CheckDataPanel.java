e0682ff74912d50d65dbf6d91b8c0fb47e27c1a5
jcbarret <jcbarret>
2006-06-13 09:51:54 +0000

    moved sorter to an inner class

--- edu/mit/wi/haploview/CheckDataPanel.java
+++ edu/mit/wi/haploview/CheckDataPanel.java
@@ -306,4 +306,19 @@ public class CheckDataPanel extends JPanel
             return cell;
         }
     }
+
+    class CheckDataTableSorter extends TableSorter {
+
+        CheckDataTableSorter(TableModel tm){
+            super(tm);
+        }
+
+        public int getRating(int row){
+            return ((CheckDataPanel.CheckDataTableModel)tableModel).getRating(modelIndex(row));
+        }
+
+        public int getDupStatus(int row){
+            return ((CheckDataPanel.CheckDataTableModel)tableModel).getDupStatus(modelIndex(row));
+        }
+    }
 }
