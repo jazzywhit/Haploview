5690cfecf9ff886923f8adf210ef9a050d80effe
djbender <djbender>
2006-09-18 16:10:47 +0000

    changed population order

--- edu/mit/wi/haploview/Constants.java
+++ edu/mit/wi/haploview/Constants.java
@@ -121,7 +121,7 @@ public interface Constants {
     //phased data stuff
     static final String[] CHROM_NAMES = {"1","2","3","4","5","6","7","8","9","10",
             "11","12","13","14","15","16","17","18","19","20","21","22","X"};
-    static final String[] POP_NAMES = {"YRI", "CEU", "CHB+JPT"};
+    static final String[] POP_NAMES = {"CEU", "YRI", "CHB+JPT"};
     static final String[] PHASE_NAMES = {"I","I+II"};
 
     //GBrowse options
