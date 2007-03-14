69f302ec6ee0b8af3b12ab19573034e690e68a19
djbender <djbender>
2007-03-14 14:29:11 +0000

    hide -debug option for now

--- edu/mit/wi/haploview/Constants.java
+++ edu/mit/wi/haploview/Constants.java
@@ -143,7 +143,6 @@ public interface Constants {
             "-n, -nogui                      Command line output only\n" +
             "-q, -quiet                      Quiet mode- doesnt print any warnings or information to screen\n" +
             "-log <filename>                 Specify a logfile name (defaults to haploview.log) \n" +
-            "-debug <filename>               Specify a debug file name (defaults to haploview.debug)\n" +
             "-pedfile <pedfile>              Specify an input file in pedigree file format\n" +
             "-hapmap <hapmapfile>            Specify an input file in HapMap format\n" +
             "-phasedhmpdata <phasedfile>     Specify a phased HapMap data file\n" +
