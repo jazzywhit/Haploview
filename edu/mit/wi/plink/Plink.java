9769a0ea895067bdd81d3db2249f093f68661337
djbender <djbender>
2007-06-01 20:04:33 +0000

    another fix for binary map files

--- edu/mit/wi/plink/Plink.java
+++ edu/mit/wi/plink/Plink.java
@@ -82,7 +82,7 @@ public class Plink {
                         }
                     }
 
-                    if (numColumns < 3){
+                    if (numColumns != 3 && numColumns != 4 && numColumns != 6){
                         throw new PlinkException("Improper map file formatting on line " + (line+1));
                     }
 
@@ -113,7 +113,7 @@ public class Plink {
                         }
                     }
                     String marker = new String(st.nextToken());
-                    if (numColumns == 4){
+                    if (numColumns == 4 || numColumns == 6){
                         //useless morgan distance
                         st.nextToken();
                     }
