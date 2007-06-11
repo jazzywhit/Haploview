3dae693d7d58fd0d9de9824ce22b93486ed79954
djbender <djbender>
2007-06-11 18:04:49 +0000

    fixed support for chrMT

--- edu/mit/wi/haploview/PlinkResultsPanel.java
+++ edu/mit/wi/haploview/PlinkResultsPanel.java
@@ -24,7 +24,7 @@ public class PlinkResultsPanel extends JPanel implements ActionListener, Constan
     private TableSorter sorter;
 
     String[] chromNames = {"","1","2","3","4","5","6","7","8","9","10",
-            "11","12","13","14","15","16","17","18","19","20","21","22","X","Y"};
+            "11","12","13","14","15","16","17","18","19","20","21","22","X","Y","XY","MT"};
     String[] signs = {"",">",">=","=","<=","<"};
     private JComboBox chromChooser, genericChooser1, genericChooser2, signChooser1, signChooser2, removeChooser;
     private NumberTextField chromStart, chromEnd;
--- edu/mit/wi/plink/Marker.java
+++ edu/mit/wi/plink/Marker.java
@@ -6,7 +6,7 @@ public class Marker {
     private short chromosome;
     private String markerID;
     private long position;
-    private static String[] chroms = {"","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y","XY"};
+    private static String[] chroms = {"","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y","XY","MT"};
 
     public Marker(short chrom, String marker, long pos){
         chromosome = chrom;
